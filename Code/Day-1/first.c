b392b4a6bc22f9ca3381a1fc831c6dc19a34deb1
roushan <roushanrhaman064@gmail.com>
2026-01-24 18:01:08 +0600

    variable learn

--- Code/Day-1/first.c
+++ Code/Day-1/first.c
@@ -3,5 +3,29 @@
 int main()
 {
     printf("Hello\n World");
+
+    // variables
+    int a = 2;
+    float b = 2.5;
+    char c = 'I';
+
+    printf("%d", a);
+    printf("%.2f", b); //.2 mane hole . er por koi ta digit dekhabe
+    printf("%c", c);
+
     return 0;
 }
+
+
+
+//variables--
+/**
+ * int -- memory te 4 byte (8 bit niye 1 byte hoy)
+ * float -- memory te 4 byte
+ * char -- 1 byte
+ * boll -- 
+ * 
+ * variable print korar jonno formate specified use korte hoy int-%d float-%f char-%c
+ */
+
+ 
