6ce006b46a0ac999dc5c5d5cd9155b6216de130c
roushan <roushanrhaman064@gmail.com>
2026-02-24 19:28:10 +0600

    print 1 to 100 using for loop

--- Code/Day-3_Loop/for_loop.c
+++ Code/Day-3_Loop/for_loop.c
@@ -2,6 +2,11 @@
 
 int main(){
     for(int i=1; i<=3; i=i+1 ){
-        printf("I am sorry\n");
+        // printf("I am sorry\n");
+    }
+    // print 1 to 100 using for loop
+
+    for(int i=0; i<=100; i++ ){
+        printf("%d\n", i);
     }
 }
