d320286b163dca051c3f8ce5b40f5dac00242cb8
roushan <roushanrhaman064@gmail.com>
2026-04-07 20:00:07 +0600

    while loop

---
+++ Code/Day-3_Loop/while_loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+
+   int n=1; //initialization 
+
+   while (n <11) //condition
+   {
+    printf("%d\n",n); //code..........
+    n++; //increment decrement
+   }
+   
+}
